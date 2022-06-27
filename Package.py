b5816b41099501a2f5e27dad9aa73e5b7d9636bc
vuvu123 <vuvu123@gmail.com>
2022-06-27 05:23:56 -0700

    add package class

--- Package.py
+++ Package.py
@@ -1,3 +1,12 @@
 class Package:
     # Package class constructor
-    def __init__(self):
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, notes):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.deadline = deadline
+        self.weight = weight
+        self.notes = notes
+
