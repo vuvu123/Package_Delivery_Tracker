045f886b8e93c883bcb3efc6e59284373f68735d
vuvu123 <vuvu123@gmail.com>
2022-06-27 18:29:15 -0700

    read distance data from csv. create graph adjacency list and edge weights

--- graph.py
+++ graph.py
@@ -1,11 +1,10 @@
+import csv
+
+
 class Graph:
     def __init__(self):
         self.adj_list = {}
         self.edge_weights = {}
-
-    def print_graph(self):
-        for vertex in self.adj_list:
-            print(vertex, ':', self.adj_list[vertex])
 
     def add_vertex(self, vertex):
         if vertex not in self.adj_list.keys():
@@ -13,9 +12,36 @@ class Graph:
             return True
         return False
 
-    def add_edge(self, v1, v2):
-        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].append(v2)
-            self.adj_list[v2].append(v1)
-            return True
-        return False
+    def add_edge(self, v1, v2, weight=None):
+        self.edge_weights[(v1, v2)] = weight
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+
+
+def load_distance_data(filename):
+    distance_data = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            distance_data.append(row)
+    return distance_data
+
+
+def create_graph(filename):
+    distance_data = load_distance_data(filename)
+    graph_distances = Graph()
+    for row in distance_data:
+        graph_distances.add_vertex(row[0])
+        for i, location in enumerate(distance_data):
+            origin = row[0]
+            destination = distance_data[i][0]
+            miles = row[i + 1]
+            graph_distances.add_edge(origin, destination, float(miles))
+    return graph_distances
+
+
+graph = create_graph('data/distances.csv')
+print(graph.edge_weights)
